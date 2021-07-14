eae034375c7beeca2d4c5cf44759c636aa6e05c2
andi.rayka <andi.rayka@gmail.com>
2021-07-14 13:28:41 +0700

    change folder structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Landing, Login } from "./components/pages";
+import { Landing, Login } from "pages";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
-      <Route path="/" component={Landing} />
+      <Route path="/" exact component={Landing} />
       <Route path="/login" component={Login} />
     </Router>
   );
