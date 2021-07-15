86a0d64cd88492dd582c6b2a8863cb0899653c41
andi.rayka <andi.rayka@gmail.com>
2021-07-15 09:03:15 +0700

    contoh penggunaan komponen

---
+++ src/components/moleculs/InputText.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// * Durung dadi, contoh tok. Monggo diganti terus komen iki dihapus
+const InputText = () => {
+  return <div>input text</div>;
+};
+
+export default InputText;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { InputText } from "@components";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <InputText />
+    </div>
+  );
 };
 
 export default Login;
