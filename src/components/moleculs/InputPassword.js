4b24f4448007a97a111265845f90a7fde462c50b
andi.rayka <andi.rayka@gmail.com>
2021-07-16 13:25:48 +0700

    add InputPassword

--- src/components/moleculs/InputPassword.js
+++ src/components/moleculs/InputPassword.js
@@ -1,18 +1,29 @@
-import React from "react";
-import { EyeIcon } from "@heroicons/react/solid";
+import React, { useState } from "react";
+import { EyeIcon, EyeOffIcon } from "@heroicons/react/solid";
 
 const InputPassword = ({ label, value, placeholder, onChange }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <div className="mt-4">
+    <div className="mt-4 bg-pink-50 relative">
       <p className="text-left text-base font-medium">{label}</p>
       <input
-        type="password"
+        type={showPassword ? "text" : "password"}
         value={value}
         placeholder={placeholder}
         onChange={({ target }) => onChange(target.value)}
         className="border-2 rounded-lg outline-none border-gray-200 px-1 py-1.5 w-full focus:border-gray-600"
       />
-      <EyeIcon className="h-5 w-5 text-blue-500" />
+      <button
+        className="absolute right-1 top-6 p-2"
+        onClick={() => setShowPassword(!showPassword)}
+      >
+        {showPassword ? (
+          <EyeOffIcon className="h-5 w-5 text-gray-500" />
+        ) : (
+          <EyeIcon className="h-5 w-5 text-gray-500" />
+        )}
+      </button>
     </div>
   );
 };
