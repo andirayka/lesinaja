ed4b638f5c378c9f53e67ecb53430e29a26e78c9
hachi-nico <nicokun48@gmail.com>
2021-07-16 11:24:10 +0700

    add custom bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,17 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend:{
+    extend: {
       colors: {
         'default-green': '#2D9A3F',
-      }
-    },    
+      },
+      backgroundImage: () => ({
+        'hero-pattern': "url('src/assets/images/logreg-logo.svg')",
+      }),
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
