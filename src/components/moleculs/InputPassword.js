640ebfc85f4659839fe542de7cf7acfbdb6e7e1d
andi.rayka <andi.rayka@gmail.com>
2021-07-16 13:09:26 +0700

    add inputText

--- src/components/moleculs/InputPassword.js
+++ src/components/moleculs/InputPassword.js
@@ -1,18 +1,20 @@
 import React from "react";
+import { EyeIcon } from "@heroicons/react/solid";
 
-const InputText = ({ label, value, placeholder, onChange }) => {
+const InputPassword = ({ label, value, placeholder, onChange }) => {
   return (
     <div className="mt-4">
       <p className="text-left text-base font-medium">{label}</p>
       <input
-        type="text"
+        type="password"
         value={value}
         placeholder={placeholder}
         onChange={({ target }) => onChange(target.value)}
         className="border-2 rounded-lg outline-none border-gray-200 px-1 py-1.5 w-full focus:border-gray-600"
       />
+      <EyeIcon className="h-5 w-5 text-blue-500" />
     </div>
   );
 };
 
-export default InputText;
+export default InputPassword;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { ContentContainer, InputText, SectionTitle, Button } from "@components";
+import {
+  ContentContainer,
+  InputText,
+  InputPassword,
+  SectionTitle,
+  Button,
+} from "@components";
 import { logregLogo } from "@assets";
 
 const Login = () => {
@@ -8,13 +14,24 @@ const Login = () => {
 
   return (
     <div className="flex flex-row justify-between items-start p-8">
-      <img src={logregLogo} className="hidden md:block m-10 w-2/4" al="" />
+      <img src={logregLogo} className="hidden md:block m-10 w-2/4" />
       <ContentContainer>
         <SectionTitle
           heading="Masuk"
           body="Ayo masuk ke akunmu untuk mulai belajar/mengajar"
         />
-        <InputText label="Email" value={email} onChange={setEmail} />
+        <InputText
+          label="Email"
+          placeholder="Contoh: handoko@gmail.com"
+          value={email}
+          onChange={setEmail}
+        />
+        <InputPassword
+          label="Kata Sandi"
+          placeholder="Masukkan kata sandi Anda"
+          value={password}
+          onChange={setPassword}
+        />
         <Button
           text="Masuk"
           additionalClassName="mt-8 bg-yellow-400 hover:bg-yellow-600 text-white w-full rounded-full"
