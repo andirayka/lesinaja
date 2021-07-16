e424e5c595d76f5efe922692e1cee27f3d0e9773
Fiqri Aidin <fikriaidin98@gmail.com>
2021-07-16 14:12:20 +0700

    Side bar

--- src/components/moleculs/SidebarItem.js
+++ src/components/moleculs/SidebarItem.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React from 'react';
+import { HomeIcon } from '@heroicons/react/solid';
 
-const SidebarItem = () => {
+const SidebarItem = ({additionalClassName, text}) => {
     return (
-        <button>Percobaan Button Item side Bar</button>
+        <button className={`py-1 rounded-md w-full text-left pl-14 ${additionalClassName}`}>
+            <HomeIcon className="w-10 inline-block" />
+            <label>{text}</label>
+        </button>
     )
 }
 
