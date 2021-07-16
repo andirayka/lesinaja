0b1eb2eb64b309858414d522353ebbf709d0feab
andi.rayka <andi.rayka@gmail.com>
2021-07-16 18:50:34 +0700

    carditem has done

--- src/components/organisms/CardItem.js
+++ src/components/organisms/CardItem.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Title } from "@components";
 
-const CardItem = ({ title, children }) => {
+const CardItem = ({ title, children, containerClass }) => {
   return (
-    <div className="rounded-md bg-white">
+    <div className={`rounded-md bg-white ${containerClass}`}>
       <Title text={title} type="cardItem" />
       <div className="p-2.5">{children}</div>
     </div>
--- src/pages/ListTutor.js
+++ src/pages/ListTutor.js
@@ -1,20 +1,35 @@
 import React from "react";
-import { Title, CardItem, CardKeyValue } from "@components";
+import { Title, CardItem, CardKeyValue, Button } from "@components";
 
 const ListTutor = () => {
   return (
     <div>
       <Title text="Daftar Tutor Lesin Aja" type="pageTitle" />
 
-      <CardItem title="Beranda Administrator">
-        <CardKeyValue keyName="Email" value="handoko@gmail.com" />
-        <CardKeyValue keyName="No. WA" value="089912345678" />
-        <CardKeyValue
-          keyName="Alamat"
-          value="Perum Graha Kuncara Blok H No.29 Kemiri , Sidoarjo, Jawa Timur, Kemiri, Kec. Sidoarjo, Kabupaten Sidoarjo, Jawa Timur 61234"
-        />
-        <CardKeyValue keyName="Mata Pelajaran" value="Matematika" />
-      </CardItem>
+      {[1, 1, 1, 1, 1, 1].map((item, key) => {
+        return (
+          <CardItem
+            key={key}
+            title="Beranda Administrator"
+            containerClass="mt-8"
+          >
+            <CardKeyValue keyName="Email" value="handoko@gmail.com" />
+            <CardKeyValue keyName="No. WA" value="089912345678" />
+            <CardKeyValue
+              keyName="Alamat"
+              value="Perum Graha Kuncara Blok H No.29 Kemiri , Sidoarjo, Jawa Timur, Kemiri, Kec. Sidoarjo, Kabupaten Sidoarjo, Jawa Timur 61234"
+            />
+            <CardKeyValue keyName="Mata Pelajaran" value="Matematika" />
+            <div className="flex-row mt-8">
+              <Button
+                text="Lihat Detail"
+                additionalClassName="bg-blue-300 rounded-lg font-medium"
+                onClick={() => {}}
+              />
+            </div>
+          </CardItem>
+        );
+      })}
     </div>
   );
 };
