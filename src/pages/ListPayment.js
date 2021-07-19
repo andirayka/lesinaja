beb731fffa81475d7f1adb81d98d5ea405b11018
andi.rayka <andi.rayka@gmail.com>
2021-07-19 08:28:21 +0700

    add listpayment

--- src/pages/ListPayment.js
+++ src/pages/ListPayment.js
@@ -7,10 +7,28 @@ const ListPayment = () => {
       <Title text="Daftar Data Master Aplikasi" type="pageTitle" />
 
       <CardItem title="Wali Murid Sucipto ke Lesin Aja" containerClass="mt-8">
+        <CardKeyValue keyName="Waktu Pembayaran" value="12 Juli 2021, 14:45" />
+        <CardKeyValue keyName="Nominal" value="Rp 200.000" />
         <div className="flex-row mt-8">
           <Button
-            text="Lihat Lebih Banyak"
+            text="Unduh Bukti Pembayaran"
             additionalClassName="bg-blue-300 rounded-lg font-medium"
+            onClick={() => {}}
+          />
+          <Button
+            text="Konfirmasi Uang Sudah Masuk"
+            additionalClassName="bg-yellow-500 rounded-lg font-medium ml-5"
+            onClick={() => {}}
+          />
+        </div>
+      </CardItem>
+      <CardItem title="Lesin Aja ke Tutor Mulyani" containerClass="mt-8">
+        <CardKeyValue keyName="Waktu Pembayaran" value="12 Juli 2021, 14:45" />
+        <CardKeyValue keyName="Nominal" value="Rp 200.000" />
+        <div className="flex-row mt-8">
+          <Button
+            text="Tandai Sudah Transfer"
+            additionalClassName="bg-yellow-500 rounded-lg font-medium"
             onClick={() => {}}
           />
         </div>
