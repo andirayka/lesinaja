db4856001502712d09406e6328fe999b5feed85a
andi.rayka <andi.rayka@gmail.com>
2021-07-21 09:16:32 +0700

    fix form master layout

--- src/components/organisms/CardFormMaster.js
+++ src/components/organisms/CardFormMaster.js
@@ -15,17 +15,20 @@ const CardFormMaster = ({ containerClass, data, onDelete }) => {
           <div key={key} className="flex flex-row py-4">
             <div className="w-3/4 ml-2.5 text-lg">{item.name}</div>
             <div className="w-1/4 flex flex-row">
-              <button className="flex-1 flex justify-center" onClick={() => {}}>
-                <PencilAltIcon className="h-8 w-8 text-gray-900" />
-              </button>
-              <button
-                className="flex-1 flex justify-center"
-                onClick={() => {
-                  onDelete(item);
-                }}
-              >
-                <TrashIcon className="h-8 w-8 text-gray-900" />
-              </button>
+              <div className="flex flex-1 justify-center">
+                <button onClick={() => {}}>
+                  <PencilAltIcon className="h-8 w-8 text-gray-900" />
+                </button>
+              </div>
+              <div className="flex flex-1 justify-center">
+                <button
+                  onClick={() => {
+                    onDelete(item);
+                  }}
+                >
+                  <TrashIcon className="h-8 w-8 text-gray-900" />
+                </button>
+              </div>
             </div>
           </div>
         );
@@ -38,17 +41,19 @@ const CardFormMaster = ({ containerClass, data, onDelete }) => {
             className="border-b-2 outline-none border-gray-300 w-4/5 focus:border-gray-600"
           />
         </div>
-        <div className="w-1/4 flex flex-row">
-          <Button
-            text="Simpan"
-            additionalClassName="bg-green-500 rounded-lg"
-            // onClick={}
-          />
-          <Button
-            text="Batal"
-            additionalClassName="bg-green-500 rounded-lg"
-            // onClick={}
-          />
+        <div className="w-1/4 flex flex-row justify-center bg-yellow-300">
+          <button
+            // onClick={onClick}
+            className="self-center bg-green-500 rounded-lg"
+          >
+            Simpan
+          </button>
+          <button
+            // onClick={onClick}
+            className="self-center bg-red-500 rounded-lg"
+          >
+            Batal
+          </button>
           {/* <button className="flex-1 flex justify-center" onClick={() => {}}>
             <PencilAltIcon className="h-8 w-8 text-gray-900" />
           </button> */}
