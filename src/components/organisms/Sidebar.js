bf3bd24d02dd98e8856060653113910295ac572f
andi.rayka <andi.rayka@gmail.com>
2021-07-21 14:02:37 +0700

    fix sidebar

--- src/components/organisms/Sidebar.js
+++ src/components/organisms/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { SidebarItem } from "@components";
 import { mainLogo } from "@assets";
@@ -12,84 +12,67 @@ import {
   faSignOutAlt,
 } from "@fortawesome/free-solid-svg-icons";
 
+const sidebarList = [
+  {
+    path: "/beranda",
+    text: "Beranda",
+    icon: faHome,
+  },
+  {
+    path: "/akun",
+    text: "Akun",
+    icon: faUser,
+  },
+  {
+    path: "/daftar-master",
+    text: "Data Master",
+    icon: faBriefcase,
+  },
+  {
+    path: "/daftar-tutor",
+    text: "Daftar Tutor",
+    icon: faChalkboardTeacher,
+  },
+  {
+    path: "/daftar-pembayaran",
+    text: "Riwayat Pembayaran",
+    icon: faMoneyCheckAlt,
+  },
+  {
+    path: "/masuk",
+    text: "Keluar",
+    icon: faSignOutAlt,
+  },
+];
+
 const Sidebar = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
-    <div className="hidden md:block flex-none w-80 bg-white rounded-md">
+    <div className="hidden md:block flex-none w-80 bg-white rounded-md overflow-hidden">
       <img src={mainLogo} alt="" className="w-64" />
       <div className="">
-        <Link to="/beranda">
-          <SidebarItem
-            text="Beranda"
-            additionalClassName="flex flex-row items-center"
-          >
-            <div className="w-12">
-              <FontAwesomeIcon icon={faHome} size="2x" className="mr-3" />
-            </div>
-          </SidebarItem>
-        </Link>
-
-        <Link to="/akun">
-          <SidebarItem
-            text="Akun"
-            additionalClassName="flex flex-row items-center"
-          >
-            <div className="w-12">
-              <FontAwesomeIcon icon={faUser} size="2x" className="mr-3" />
-            </div>
-          </SidebarItem>
-        </Link>
-
-        <Link to="/daftar-master">
-          <SidebarItem
-            text="Data Master"
-            additionalClassName="flex flex-row items-center"
-          >
-            <div className="w-12">
-              <FontAwesomeIcon icon={faBriefcase} size="2x" className="mr-3" />
-            </div>
-          </SidebarItem>
-        </Link>
-
-        <Link to="/daftar-tutor">
-          <SidebarItem
-            text="Daftar Tutor"
-            additionalClassName="flex flex-row items-center"
-          >
-            <div className="w-12">
-              <FontAwesomeIcon
-                icon={faChalkboardTeacher}
-                size="2x"
-                className="mr-3"
-              />
-            </div>
-          </SidebarItem>
-        </Link>
-
-        <Link to="/daftar-pembayaran">
-          <SidebarItem
-            text="Riwayat Pembayaran"
-            additionalClassName="flex flex-row items-center"
-          >
-            <div className="w-12">
-              <FontAwesomeIcon
-                icon={faMoneyCheckAlt}
-                size="2x"
-                className="mr-3"
-              />
-            </div>
-          </SidebarItem>
-        </Link>
-
-        <Link to="/masuk">
-          <SidebarItem
-            text="Keluar"
-            additionalClassName="flex flex-row items-center"
-          >
-            <div className="w-12">
-              <FontAwesomeIcon icon={faSignOutAlt} size="2x" className="mr-3" />
-            </div>
-          </SidebarItem>
-        </Link>
+        {sidebarList.map((item, index) => {
+          return (
+            <Link
+              key={index}
+              to={item.path}
+              onClick={() => {
+                setActiveIndex(index);
+              }}
+            >
+              <SidebarItem
+                isActive={activeIndex == index}
+                text={item.text}
+                additionalClassName="flex flex-row items-center"
+              >
+                <div className="w-12 mr-2">
+                  <FontAwesomeIcon icon={item.icon} size="2x" />
+                </div>
+              </SidebarItem>
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
