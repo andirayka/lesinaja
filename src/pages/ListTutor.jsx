235595beac708d3e1f81f25a7703bfe61549c33a
Fiqri Aidin <fikriaidin98@gmail.com>
2021-07-30 18:27:55 +0700

    fix list tutor om

--- src/pages/ListTutor.jsx
+++ src/pages/ListTutor.jsx
@@ -1,29 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Title, CardItem, CardKeyValue, Button } from "@components";
 import { Link } from "react-router-dom";
 import { getFirebaseDataOnce } from "@utils";
 
 const ListTutor = () => {
-  const getDataOm = () => {
-    getFirebaseDataOnce({ ref });
-    console.log(getFirebaseDataOnce);
+  const [data, setData] = useState({});
+
+  const getDataFirebase = async () => {
+    const getData = await getFirebaseDataOnce({ ref: `user_role/tutor` });
+    // console.log(getData);
+    setData(getData);
   };
+
+  useEffect(() => {
+    getDataFirebase();
+  }, []);
 
   return (
     <div className="w-full flex-grow md:ml-8">
       <Title text="Daftar Tutor Lesin Aja" type="pageTitle" />
-
-      {[1, 1, 1, 1, 1, 1].map((item, key) => {
+      {Object.entries(data).map((item, index) => {
+        const [key, value] = item;
+        // console.log(key);
         return (
-          <CardItem key={key} title="Abdul Majid, S.Kom." containerClass="mt-8">
-            <CardKeyValue keyName="Email" value="handoko@gmail.com" />
-            <CardKeyValue keyName="No. WA" value="089912345678" />
-            <CardKeyValue
-              keyName="Alamat"
-              value="Perum Graha Kuncara Blok H No.29 Kemiri , Sidoarjo, Jawa Timur, Kemiri, Kec. Sidoarjo, Kabupaten Sidoarjo, Jawa Timur 61234"
-            />
-            <CardKeyValue keyName="Mata Pelajaran" value="Matematika" />
-            <button onClick={getDataOm}>Percobaan</button>
+          <CardItem key={index} title={value.nama} containerClass="mt-8">
+            <CardKeyValue keyName="Email" value={value.email} />
+            <CardKeyValue keyName="No. WA" value={value.nomor} />
+            <CardKeyValue keyName="Alamat" value={value.alamat} />
+            <CardKeyValue keyName="Mata Pelajaran" value={value.matkul} />
             <div className="flex-row mt-8">
               <Link
                 to={{
