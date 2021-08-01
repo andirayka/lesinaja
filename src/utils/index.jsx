0e6566027cb782041f3b6d1dafff52a716d39b83
Fiqri Aidin <fikriaidin98@gmail.com>
2021-08-01 23:24:42 +0700

    fix reset password om

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -6,9 +6,11 @@ import {
   deleteFirebaseData,
   handleRegister,
   handleLogin,
+  handleResetPassword,
 } from "./firebaseConfig";
 
 export {
+  handleResetPassword,
   handleLogin,
   handleRegister,
   enableFirebaseConfig,
