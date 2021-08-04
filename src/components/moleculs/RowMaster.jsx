d19cc1af5e7dc55c37854304baac0a220b27d316
hachi-nico <nicokun48@gmail.com>
2021-08-04 14:41:48 +0700

    fix display data on form master wilayah, add some styles

--- src/components/moleculs/RowMaster.jsx
+++ src/components/moleculs/RowMaster.jsx
@@ -93,8 +93,16 @@ const RowMaster = ({
                 data={dropdownData}
                 prompt={
                   multipleItem.length !== 0
-                    ? multipleItem.map((item) => {
-                        return `${item.nama} `;
+                    ? multipleItem.map((item, index) => {
+                        return (
+                          <div
+                            key={index}
+                            className="inline-block bg-gray-300 rounded-md m-1 p-1"
+                            onClick={() => console.log("d")}
+                          >
+                            {`${item.nama} `} &#10006;
+                          </div>
+                        );
                       })
                     : "Pilih Provinsi..."
                 }
@@ -236,12 +244,19 @@ const RowMaster = ({
 
     if (formName == "master_wilayah") {
       return (
-        // <>
-        //   <div className="w-3/4 ml-2.5 text-lg">{item.nama}</div>
-        //   <div className="w-3/4 ml-2.5 text-lg">{item.biaya_daftar}</div>
-        //   <div className="w-3/4 ml-2.5 text-lg">{item.provinsi.nama}</div>
-        // </>
-        <div></div>
+        <>
+          <div className="w-3/4 ml-2.5 text-lg">{item.nama}</div>
+          <div className="w-3/4 ml-2.5 text-lg">{item.biaya_daftar}</div>
+          <div className="w-3/4 ml-2.5 text-lg">
+            {item.provinsi.map((item, index) => {
+              return (
+                <div key={index} className="bg-gray-300 rounded-md m-1 p-1">
+                  {item.nama}
+                </div>
+              );
+            })}
+          </div>
+        </>
       );
     }
   };
