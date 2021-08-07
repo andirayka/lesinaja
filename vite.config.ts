70164c3ef7ff40a9857415512c7ee220cd43e45b
andi.rayka <andi.rayka@gmail.com>
2021-08-07 22:30:48 +0700

    setup lanjutan

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [reactRefresh()],
+  resolve: {
+    alias: [
+      { find: "@components", replacement: "/src/components" },
+      { find: "@pages", replacement: "/src/pages" },
+      { find: "@context", replacement: "/src/context" },
+      { find: "@utils", replacement: "/src/utils" },
+      { find: "@assets", replacement: "/src/assets" },
+    ],
+  },
+});
